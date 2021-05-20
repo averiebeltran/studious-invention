cdbafe18ad2957a0d0e910db287bc1c8f522d803
AverieBeltran <averiebeltran@github.com>
2021-05-20 09:40:52 -0500

    added variable to server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,12 @@ const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
